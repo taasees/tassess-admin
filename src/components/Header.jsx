47a93f226bf85dcf8e01531f8a45ed67175ce71e
hussamshannan <info@shark-plan.com>
2025-07-15 18:51:20 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -413,7 +413,10 @@ export default function Header() {
       <div ref={sidebarRef} className={`side ${isSideOpen ? "show" : ""}`}>
         <div className="top">
           <Link to="/" className="logo">
-            <img src={logo} alt="logo" />
+            <img
+              src={logoFile ? URL.createObjectURL(logoFile) : menuTxt.logoUrl}
+              alt="logo"
+            />
           </Link>
           <div className="menu close" onClick={toggleSidebar}>
             {closeMenu}
